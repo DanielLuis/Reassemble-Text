809240e5eee0e46cd8757c9f30ad5f3d37d65f42
daniel <dluis2007@gmail.com>
2023-04-13 16:04:43 +0100

    Reassemble-Text
    Challenge: Reassemble Text Fragments
    Background
    Imagine you have 5 copies of the same page of text. You value this text and have no hard or soft
    copies of it. Your two year old nephew visits and, while you are not looking, rips each page up into
    fragments and gleefully plays in the “snow” he has just created.
    You need at least one copy of that page of text back ASAP. As punishment to your niece, who should
    have been supervising your nephew at the time of the incident, you set her the painstaking task of
    keying in all the paper text fragments to a text file on your shiny MacBook Pro. Now the task is
    yours. Can you reassemble a soft copy of the original document?
    The Challenge
    Write a program to reassemble a given set of text fragments into their original sequence. For this
    challenge your program should have a main method accepting one argument – the path to a well-
    formed UTF-8 encoded text file. Each line in the file represents a test case of the main functionality
    of your program: read it, process it and println to the console the corresponding defragmented
    output.
    Each line contains text fragments separated by a semicolon, ‘;’. You can assume that every fragment
    has length at least 2.
    Example input 1:
    O draconia;conian devil! Oh la;h lame sa;saint!
    
    Example output 1:
    O draconian devil! Oh lame saint!
    
    Example input 2:
    m quaerat voluptatem.;pora incidunt ut labore et d;, consectetur, adipisci velit;olore magnam
    aliqua;idunt ut labore et dolore magn;uptatem.;i dolorem ipsum qu;iquam quaerat vol;psum quia
    dolor sit amet, consectetur, a;ia dolor sit amet, conse;squam est, qui do;Neque porro quisquam est,
    qu;aerat voluptatem.;m eius modi tem;Neque porro qui;, sed quia non numquam ei;lorem ipsum
    quia dolor sit amet;ctetur, adipisci velit, sed quia non numq;unt ut labore et dolore magnam aliquam
    qu;dipisci velit, sed quia non numqua;us modi tempora incid;Neque porro quisquam est, qui
    dolorem i;uam eius modi tem;pora inc;am al
    
    Example output 2:
    
    Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
    quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
    voluptatem.

--- src/main/java/ReassembleText.java
+++ src/main/java/ReassembleText.java
@@ -1,4 +1,166 @@
 package src.main.java;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static java.util.function.Predicate.not;
+
 public class ReassembleText {
+    private static final String VALIDATION_ERROR = "Invalid args";
+    private static final String EMPTY_VALUE = "";
+    private static final String SEPARATOR = ";";
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+
+    public static void main(String[] args) {
+        if (args.length == ZERO || args[ZERO] == null) {
+            throw new RuntimeException(VALIDATION_ERROR);
+        }
+
+        try (BufferedReader in = new BufferedReader(new FileReader(args[ZERO]))) {
+            in.lines()
+                    .filter(Objects::nonNull)
+                    .filter(not(String::isBlank))
+                    .map(ReassembleText::reassemble)
+                    .forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method to create a set of strings based in one single line (separator = ;)
+     *
+     * @param fragment string line from document
+     * @return {@link List<String>}
+     */
+    private static List<String> splitValues(String fragment) {
+        return Arrays.stream(fragment.split(SEPARATOR))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * reassemble fragment line
+     *
+     * @param fragment line fragment
+     * @return {@link String}
+     */
+    public static String reassemble(final String fragment) {
+        List<String> fragments = splitValues(fragment);
+
+        if (fragments.size() == ONE) {
+            return fragment;
+        }
+
+        while (fragments.size() > ONE) {
+            String concatenatedString = null;
+            int numberOfOverlappingCharacters = ZERO;
+            int[] selectedVariation = new int[2];
+
+            for (int[] variation : getAllPairVariations(fragments.size())) {
+                String[] concatResult = concatWithOverlappingCharacters(fragments.get(variation[ZERO]), fragments.get(variation[ONE]));
+                if (Integer.parseInt(concatResult[ONE]) > numberOfOverlappingCharacters) {
+                    selectedVariation = new int[]{variation[ZERO], variation[ONE]};
+                    numberOfOverlappingCharacters = Integer.parseInt(concatResult[ONE]);
+                    concatenatedString = concatResult[ZERO];
+                }
+            }
+
+            removeFragments(fragments, selectedVariation);
+            fragments.add(Objects.nonNull(concatenatedString) ? concatenatedString : "<<No Overlaping>>");
+        }
+
+        return fragments.get(ZERO);
+    }
+
+    /**
+     * build all possible pair of two based on fragments size
+     *
+     * @param size fragments size
+     * @return {@link List}
+     */
+    public static List<int[]> getAllPairVariations(int size) {
+        List<int[]> pair = new ArrayList<>();
+
+        int index = ZERO;
+
+        while (index < size) {
+            for (int i = ZERO; i < size; i++) {
+                if (i != index) pair.add(new int[]{index, i});
+            }
+            index++;
+        }
+
+        return pair;
+    }
+
+    /**
+     * remove two compared pairs with max overlaping
+     *
+     * @param fragments fragments
+     * @param variation selected variation
+     */
+    public static void removeFragments(List<String> fragments, int[] variation) {
+        List<Integer> indicesToRemove = Arrays.asList(variation[ZERO], variation[ONE]);
+        indicesToRemove.sort(Collections.reverseOrder());
+        indicesToRemove.forEach(index -> fragments.remove(index.intValue()));
+    }
+
+    /**
+     * Overlapping chars comparing two strings
+     *
+     * @param firstString  first string
+     * @param secondString second string
+     * @return {@link String[]}
+     */
+    public static String[] concatWithOverlappingCharacters(final String firstString, final String secondString) {
+        if (Objects.isNull(firstString) && Objects.isNull(secondString)) {
+            return new String[]{EMPTY_VALUE, String.valueOf(ZERO)};
+        }
+
+        if (Objects.nonNull(firstString) && Objects.isNull(secondString)) {
+            return new String[]{firstString, String.valueOf(ZERO)};
+        }
+
+        if (Objects.isNull(firstString)) {
+            return new String[]{secondString, String.valueOf(ZERO)};
+        }
+
+        if (firstString.contains(secondString)) {
+            return new String[]{firstString, String.valueOf(secondString.length())};
+        }
+
+
+        int string1Length = firstString.length() - ONE;
+        int string2Length = secondString.length() - ONE;
+        char lastCharAtString1 = firstString.charAt(string1Length);
+        char firstCharAtString2 = secondString.charAt(ZERO);
+
+        int index = secondString.lastIndexOf(lastCharAtString1, Math.min(string1Length, string2Length));
+
+        while (index != -1) {
+            if (firstString.charAt(string1Length - index) == firstCharAtString2) {
+                int i = index;
+
+                while ((i != -1) && (firstString.charAt(string1Length - index + i) == secondString.charAt(i))) {
+                    i--;
+                }
+
+                if (i == -1) {
+                    return new String[]{firstString + secondString.substring(index + ONE), String.valueOf(index + ONE)};
+                }
+            }
+            index = secondString.lastIndexOf(lastCharAtString1, index - ONE);
+        }
+        return new String[]{firstString + secondString, String.valueOf(ZERO)};
+    }
 }
+
+
